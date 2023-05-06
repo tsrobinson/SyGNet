b8249f1aaa0115142b494ca50623e20f95a92d75
antndlcrx <91369103+antndlcrx@users.noreply.github.com>
2023-05-06 19:37:27 +0100

    add sdv quality report

--- src/sygnet/requirements.py
+++ src/sygnet/requirements.py
@@ -9,6 +9,7 @@ import torch.optim as optim
 from torch.autograd import Variable
 from torch.utils.data import Dataset, DataLoader
 import torch.nn.functional as F
+from sdmetrics.reports.single_table import QualityReport
 
 import pandas as pd
 from datetime import datetime
